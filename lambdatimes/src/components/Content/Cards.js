03a8de897fdd23b3bad11f6682e2586ea766107e
Timothy Shores <timothymshores@gmail.com>
2019-02-22 12:42:37 -0600

    Refactored CSS in Content, Tabs, Cards, Card and App to styled components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from './Card';
+import styled from 'styled-components';
+
+const CardsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardsContainer>
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
       {props.cards.map((card, index) => {
@@ -19,7 +33,7 @@ const Cards = props => {
           />
         );
       })}
-    </div>
+    </CardsContainer>
   )
 }
 
