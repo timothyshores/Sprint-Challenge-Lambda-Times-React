e9ddc992855bd501b6a39742e41dd73749a724b4
Timothy Shores <timothymshores@gmail.com>
2019-02-22 12:15:32 -0600

    Refactored Header.js and TopBar.js to styled components

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Tabs from './Tabs';
 import Cards from './Cards';
 
@@ -43,18 +42,11 @@ export default class Content extends Component {
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
 
-
     if (this.state.selected === 'all') {
       return this.state.cards;
     } else {
-      return this.state.cards.filter(
-        (card) => card.tab === this.state.selected
-      );
+      return this.state.cards.filter(card => card.tab === this.state.selected);
     }
-
-    //   (this.state.selected == 'all') ? this.state.cards : {(card) => card.tab == this.state.selected
-    // }
-
   };
 
   render() {
