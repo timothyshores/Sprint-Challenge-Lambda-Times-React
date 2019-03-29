6d01b7fbec0c6a647f378c115d26e37151c00d50
Timothy Shores <timothymshores@gmail.com>
2019-03-29 10:35:25 -0500

    Map over cards prop and create new card component with props

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const Card = props => {
-  return (
-    <div className="card">
-      <div className="headline">{/* headline goes here */}</div>
-      <div className="author">
-        <div className="img-container">
-          <img src={'' /* image source goes here */} />
+    console.log('Card props', props);
+    return (
+        <div className="card">
+            <div className="headline">{/* headline goes here */}</div>
+            <div className="author">
+                <div className="img-container">
+                    <img src={'' /* image source goes here */} />
+                </div>
+                <span>By {/* author goes here */}</span>
+            </div>
         </div>
-        <span>By {/* author goes here */}</span>
-      </div>
-    </div>
-  );
+    );
 };
 
 // Make sure to include PropTypes.
