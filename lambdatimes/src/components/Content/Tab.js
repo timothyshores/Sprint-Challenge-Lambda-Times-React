1ff4684956189fe22abef1a0caf3e3c99483f5db
Timothy Shores <timothymshores@gmail.com>
2019-03-29 12:46:31 -0500

    Add propTypes to Cards and Card components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -5,7 +5,6 @@ const Tab = props => {
     /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
         if they match, the className should be: 'tab active-tab', 
         if it is not it should just be 'tab'*/
-    console.log('Tab props', props)
     let tabClass = 'tab';
     if (props.tab === props.selectedTab) {
         tabClass = 'tab active-tab';
